bbbe933dac2f5146b7616295f3e50c3f5b94bd20
Sakar Pudasaini <barcaboysakar@gmail.com>
2025-04-03 15:49:48 +0545

    git init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <div
+        className=" text-red-400
+      "
+      >
+        sakar Pudasaini
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: ".9375rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1230px",
+        },
+      },
+
+      colors: {
+        primary: "#307bbf",
+        secondary: "f57f21",
+        "primary-dark": "#174c8d",
+        third: "4f94d3",
+        muted: "686868",
+        headings: "#001221",
+        "body-bg": "#fdfeff",
+      },
+      fontFamily: {
+        primary: ["var(--primary)"],
+        secondary: ["var(--secondary)"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
